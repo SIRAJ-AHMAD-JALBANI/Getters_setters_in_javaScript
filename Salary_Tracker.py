319481f3fc51f2f631576cc6cf8b763f49646c88
SIRAJ-AHMAD-JALBANI <s4495942@gmail.com>
2026-01-03 20:16:18 +0500

    Salary Tracker in python

--- Salary_Tracker.py
+++ Salary_Tracker.py
@@ -1,5 +1,24 @@
 class Employee:
-    def __init__(self,name,level):
-        self.name = name
-        self.level = level
-        
+    
+    def __init__(self, name, level):
+        if not (isinstance(name, str) and isinstance(level, str)):
+            raise TypeError("'name' and 'level' attribute must be of type 'str'.")
+        self._name = name
+        self._level = level
+
+    def __str__(self):
+        return f'{self.name}: {self.level}'
+
+    def __repr__(self):
+        return f"Employee('{self.name}', '{self.level}')"
+
+    @property
+    def name(self):
+        return self._name
+
+    @property
+    def level(self):
+        return self._level
+
+charlie_brown = Employee('Charlie Brown', 'trainee')
+print(charlie_brown)
